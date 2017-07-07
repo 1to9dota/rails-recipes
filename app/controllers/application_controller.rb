45dec0cc7b939a896cb5f1535fd51328f56072da
1to9 <251838931@qq.com>
2017-07-07 15:23:57 +0800

    shiqu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_timezone
+
+   def set_timezone
+     if current_user && current_user.time_zone
+       Time.zone = current_user.time_zone
+     end
+   end
   before_action :set_locale
 
  def set_locale
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-
+resource :user
   resources :events
 
   namespace :admin do
