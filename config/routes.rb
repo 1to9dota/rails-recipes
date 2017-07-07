2ec3cc659bad2936c239e19e2022027343bb59f1
1to9 <251838931@qq.com>
2017-07-08 07:37:25 +0800

    17-3

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
   resource :user
   resources :events do
-      resources :registrations
+    resources :registrations do
+      member do
+        get "steps/1" => "registrations#step1", :as => :step1
+        patch "steps/1/update" => "registrations#step1_update", :as => :update_step1
+        get "steps/2" => "registrations#step2", :as => :step2
+        patch "steps/2/update" => "registrations#step2_update", :as => :update_step2
+        get "steps/3" => "registrations#step3", :as => :step3
+        patch "steps/3/update" => "registrations#step3_update", :as => :update_step3
+      end
     end
+end
+
   namespace :admin do
     root "events#index"
     resources :events do
@@ -17,6 +27,7 @@ Rails.application.routes.draw do
          post :reorder
        end
     end
+
     resources :users do
       resource :profile, :controller => "user_profiles"
     end
